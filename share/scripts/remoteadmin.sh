4238d6f1e5bca629b89b05f4eb04780db00d16a8
Thomas Schmitt <schmitt@lmz-bw.de>
2010-01-19 12:48:11 +0000

    * remoteadmin-Skript überarbeitet, wird jetzt als Systembenutzer angelegt, nicht mehr als ldap-Benutzer, Optionen --activate und --deactivate entfernt (#153).  * Bugfix: Kein Zugriff auf netlogon-Unterordner nach Paket-Upgrade von linuxmuster-base (#182).

--- share/scripts/remoteadmin.sh
+++ share/scripts/remoteadmin.sh
@@ -2,10 +2,11 @@
 #
 # create and remove a remote administrator
 #
-# 03.07.2008
+# 18.01.2010
 # Thomas Schmitt
 # <schmitt@lmz-bw.de>
-#
+# GPL v3
+# 
 
 # source linxmuster environment
 . /usr/share/linuxmuster/config/dist.conf
@@ -24,7 +25,6 @@ sudoerstr[1]="$REMOTEADMIN ALL=(ALL) ALL ## DO NOT EDIT! ##"
 sudoerstr[2]="## end linuxmuster $REMOTEADMIN ## DO NOT EDIT! ##"
 
 remove_quota_entry() {
-
 	# remove old entry if there is one
 	for i in $QUOTACONF $MAILQUOTACONF; do
 		if grep -qw ^$REMOTEADMIN $i; then
@@ -32,83 +32,18 @@ remove_quota_entry() {
 			mv $tmpdir/quota.tmp $i
 		fi
 	done
-
-}
-
-set_quota_entry() {
-
-	remove_quota_entry
-
-	for i in $QUOTACONF $MAILQUOTACONF; do
-
-		# determine administrator quota
-		unset quota_value
-		quota_value=`grep -w ^$ADMINISTRATOR $i | awk '{ print $2 }'`
-
-		# writing new quota value for remoteadmin
-		[ -n "$quota_value" ] && echo "$REMOTEADMIN: $quota_value" >> $i
-
-	done
-
-}
-
-create_account() {
-
-	sophomorix-useradd --administrator $REMOTEADMIN --unix-group $DOMADMINS --shell /bin/sh --gecos "Remote Administrator" &> /dev/null
-	sophomorix-passwd --interactive --user $REMOTEADMIN
-	smbldap-usermod -H '[UX         ]' $REMOTEADMIN
-	set_quota_entry
-	$SCRIPTSDIR/cyrus-mbox -q $quota_value -c $REMOTEADMIN
-	sophomorix-quota --force --set -u $REMOTEADMIN &> /dev/null
-
-}
-
-create_cert() {
-
-	pwstatus=0
-	while [ "$pwstatus" = "0" ]; do
-		stty -echo
-		read -p "Please enter certificate password (6 chars at least): " certpw; echo
-		len=${#certpw}
-		if [ $len -lt 6 ]; then
-			stty echo
-			echo "Password too short! Six (6) characters at least!"
-		else
-			read -p "Please confirm certificate password: " certpw_confirm; echo
-			stty echo
-			if [ "$certpw" = "$certpw_confirm" ]; then
-				pwstatus=1
-			else
-				echo "Passwords do not match!"
-			fi
-		fi
-	done
-
-	linuxmuster-ovpn --create --username=$REMOTEADMIN --password="$certpw"
-	linuxmuster-ovpn --activate --username=$REMOTEADMIN
-
-}
-
-remove_account() {
-
-	sophomorix-kill --killuser $REMOTEADMIN  &> /dev/null
-	remove_quota_entry
-
 }
 
 add_to_sudoers() {
-
 	cp /etc/sudoers /etc/sudoers.${REMOTEADMIN}.backup.add
 	for i in 0 1 2; do
 		echo "${sudoerstr[$i]}" >> /etc/sudoers
 	done
 	chown root:root /etc/sudoers*
 	chmod 440 /etc/sudoers*
-
 }
 
 add_to_nagios() {
-
 	cp /etc/nagios2/cgi.cfg /etc/nagios2/cgi.cfg.${REMOTEADMIN}.backup.add
 	cp /etc/nagios2/apache2.conf /etc/nagios2/apache2.conf.${REMOTEADMIN}.backup.add
 	if grep -q ${REMOTEADMIN} /etc/nagios2/cgi.cfg; then
@@ -124,11 +59,9 @@ add_to_nagios() {
 	fi
 	/etc/init.d/nagios2 restart
 	/etc/init.d/apache2 restart
-
 }
 
 remove_from_nagios() {
-
 	cp /etc/nagios2/cgi.cfg /etc/nagios2/cgi.cfg.${REMOTEADMIN}.backup.remove
 	cp /etc/nagios2/apache2.conf /etc/nagios2/apache2.conf.${REMOTEADMIN}.backup.remove
 	sed -e "s/=${REMOTEADMIN},/=/g" -i /etc/nagios2/cgi.cfg
@@ -136,11 +69,9 @@ remove_from_nagios() {
 	sed -e "s/ ${REMOTEADMIN}//g" -i /etc/nagios2/apache2.conf
 	/etc/init.d/nagios2 restart
 	/etc/init.d/apache2 restart
-
 }
 
 remove_from_sudoers() {
-
 	cp /etc/sudoers /etc/sudoers.${REMOTEADMIN}.backup.remove
 	for i in 0 1 2; do
 		grep -v "${sudoerstr[$i]}" /etc/sudoers > $tmpdir/sudoers
@@ -148,22 +79,19 @@ remove_from_sudoers() {
 	done
 	chown root:root /etc/sudoers*
 	chmod 440 /etc/sudoers*
-
 }
 
 add_to_webmin() {
-
 	cp /etc/webmin/webmin.acl /etc/webmin/webmin.acl.${REMOTEADMIN}.backup.add
 	cp /etc/webmin/miniserv.users /etc/webmin/miniserv.users.${REMOTEADMIN}.backup.add
 	echo "${REMOTEADMIN}: acl change-user servers webmin webminlog apache at custom cron fdisk init inittab man mount net pam passwd proc raid shell syslog time useradmin filemanager htaccess inetd logrotate mailboxes mysql postgresql software spamassassin status updown fetchmail grub lvm sshd webalizer" >> /etc/webmin/webmin.acl
 	echo "${REMOTEADMIN}:x:0::" >> /etc/webmin/miniserv.users
 	chown root:shadow /etc/webmin/miniserv.users*
 	chmod 750 /etc/webmin/miniserv.users*
-
+	/etc/init.d/webmin restart &> /dev/null
 }
 
 remove_from_webmin() {
-
 	cp /etc/webmin/webmin.acl /etc/webmin/webmin.acl.${REMOTEADMIN}.backup.remove
 	cp /etc/webmin/miniserv.users /etc/webmin/miniserv.users.${REMOTEADMIN}.backup.remove
 	grep -v ${REMOTEADMIN} /etc/webmin/webmin.acl > $tmpdir/webmin.acl
@@ -172,11 +100,10 @@ remove_from_webmin() {
 	mv $tmpdir/miniserv.users /etc/webmin
 	chown root:shadow /etc/webmin/miniserv.users*
 	chmod 750 /etc/webmin/miniserv.users*
-
+	/etc/init.d/webmin restart &> /dev/null
 }
 
 do_accessconf() {
-
 	allowed=`grep ^"-:ALL EXCEPT" /etc/security/access.conf`
 	allowed=${allowed#-:ALL EXCEPT }
 	allowed=${allowed%:ALL}
@@ -191,9 +118,33 @@ do_accessconf() {
 	allowed=${allowed% }
 	sedstr="-:ALL EXCEPT ${allowed}:ALL"
 	sedstr=${sedstr//  / }
-	sed -e "s/^-:ALL EXCEPT.*/$sedstr/" /etc/security/access.conf > $tmpdir/access.conf
-	mv $tmpdir/access.conf /etc/security
+	sed -e "s/^-:ALL EXCEPT.*/$sedstr/" -i /etc/security/access.conf
+}
 
+create_account() {
+ useradd -c "Remote Admin" -d $ADMINSHOME/$REMOTEADMIN -r -s /bin/bash $REMOTEADMIN
+ mkdir -p $ADMINSHOME/$REMOTEADMIN
+ chown $REMOTEADMIN:$REMOTEADMIN $ADMINSHOME/$REMOTEADMIN -R
+ chmod 700 $ADMINSHOME/$REMOTEADMIN
+ passwd $REMOTEADMIN
+ add_to_nagios
+ add_to_sudoers
+ add_to_webmin
+	do_accessconf add
+}
+
+remove_account() {
+ remove_from_nagios
+ remove_from_sudoers
+ remove_from_webmin
+	do_accessconf
+ if check_id $REMOTEADMIN; then
+	 sophomorix-kill --killuser $REMOTEADMIN &> /dev/null
+	 remove_quota_entry
+ else
+  id $REMOTEADMIN &> /dev/null && deluser $REMOTEADMIN
+ fi
+ rm -rf $ADMINSHOME/$REMOTEADMIN
 }
 
 cat /etc/issue
@@ -201,101 +152,43 @@ cat /etc/issue
 case $1 in
 
 	--create)
-		echo "Creating account for $REMOTEADMIN ..."
-		check_id $REMOTEADMIN && remove_account
-		create_account
-		linuxmuster-ovpn --check --username=$REMOTEADMIN && linuxmuster-ovpn --purge --username=$REMOTEADMIN
-		create_cert
-		grep -q $REMOTEADMIN /etc/sudoers && remove_from_sudoers
-		add_to_sudoers
-		do_accessconf add
-		grep -q $REMOTEADMIN /etc/webmin/* && remove_from_webmin
-		add_to_webmin
-		/etc/init.d/webmin restart &> /dev/null
-		add_to_nagios
-		if check_id $REMOTEADMIN; then
-			echo "Account for $REMOTEADMIN successfully created!"
-			status=0
-		else
-			echo "Failed to create account for $REMOTEADMIN!"
-			remove_account
-			grep -q $REMOTEADMIN /etc/sudoers && remove_from_sudoers
-			do_accessconf
-			status=1
-		fi
+		if id $REMOTEADMIN &> /dev/null; then
+   echo "There is already an account for $REMOTEADMIN!"
+   status=1
+  else
+ 		echo "Creating account for $REMOTEADMIN ..."
+ 		create_account
+ 		if id $REMOTEADMIN &> /dev/null; then
+ 			echo "Account for $REMOTEADMIN successfully created!"
+ 			status=0
+ 		else
+ 			echo "Failed to create account for $REMOTEADMIN!"
+ 			remove_account
+ 			status=1
+ 		fi
+  fi
 		;;
 
 	--remove)
-		echo "Removing account for $REMOTEADMIN ..."
-		linuxmuster-ovpn --check --username=$REMOTEADMIN && linuxmuster-ovpn --purge --username=$REMOTEADMIN
-		check_id $REMOTEADMIN && remove_account
-		grep -q $REMOTEADMIN /etc/sudoers && remove_from_sudoers
-		do_accessconf
-		grep -q $REMOTEADMIN /etc/webmin/* && remove_from_webmin
-		/etc/init.d/webmin restart &> /dev/null
-		remove_from_nagios
-		if check_id $REMOTEADMIN; then
-			echo "Failed to remove account for $REMOTEADMIN!"
-			status=1
-		else
-			echo "Account for $REMOTEADMIN successfully removed!"
-			status=0
-		fi
-		;;
-
-	--activate)
-		echo "Activating $REMOTEADMIN account ..."
-		if check_id $REMOTEADMIN; then
-			if sophomorix-passwd --interactive --user $REMOTEADMIN; then
-				smbldap-usermod -H '[UX         ]' $REMOTEADMIN
-				if linuxmuster-ovpn --check --username=$REMOTEADMIN; then
-					linuxmuster-ovpn --activate --username=$REMOTEADMIN
-				else
-					create_cert
-				fi
-				grep -q $REMOTEADMIN /etc/sudoers && remove_from_sudoers
-				add_to_sudoers
-				do_accessconf add
-				grep -q $REMOTEADMIN /etc/webmin/* && remove_from_webmin
-				add_to_webmin
-				/etc/init.d/webmin restart &> /dev/null
-				echo "Account for $REMOTEADMIN successfully activated!"
-				status=0
-			else
-				echo "Failed to activate account for $REMOTEADMIN!"
-				status=1
-			fi
-		else
-			echo "Account for $REMOTEADMIN does not exist!"
-			status=1
-		fi
-		;;
-
-	--deactivate)
-		echo "Deactivating $REMOTEADMIN account ..."
-		if check_id $REMOTEADMIN; then
-			secret_passwd=`pwgen -s 8 1`
-			if sophomorix-passwd --user $REMOTEADMIN --pass "$secret_passwd" &> /dev/null; then
-				linuxmuster-ovpn --deactivate --username=$REMOTEADMIN
-				smbldap-usermod -H '[DUX        ]' $REMOTEADMIN
-				grep -q $REMOTEADMIN /etc/sudoers && remove_from_sudoers
-				do_accessconf
-				grep -q $REMOTEADMIN /etc/webmin/* && remove_from_webmin
-				/etc/init.d/webmin restart &> /dev/null
-				echo "Account for $REMOTEADMIN successfully deactivated!"
-				status=0
-			else
-				echo "Failed to deactivate account for $REMOTEADMIN!"
-				status=1
-			fi
-		else
-			echo "Account for $REMOTEADMIN does not exist!"
-			status=1
-		fi
+		if ! id $REMOTEADMIN &> /dev/null; then
+   echo "There is no user $REMOTEADMIN!"
+   status=1
+  else
+ 		echo "Removing account for $REMOTEADMIN ..."
+ 		linuxmuster-ovpn --check --username=$REMOTEADMIN &> /dev/null && linuxmuster-ovpn --purge --username=$REMOTEADMIN
+   remove_account
+ 		if id $REMOTEADMIN &> /dev/null; then
+ 			echo "Failed to remove account for $REMOTEADMIN!"
+ 			status=1
+ 		else
+ 			echo "Account for $REMOTEADMIN successfully removed!"
+ 			status=0
+ 		fi
+  fi
 		;;
 
 	*)
-		echo "Usage: $0 [--create|--activate|--remove|--deactivate]"
+		echo "Usage: $0 [--create|--remove]"
 		;;
 
 esac
@@ -305,3 +198,4 @@ rm -rf $tmpdir
 rm -f $locker
 
 exit $status
+
