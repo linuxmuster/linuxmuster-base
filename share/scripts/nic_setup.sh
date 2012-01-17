80a639e71970353c0f4bae6817549adae436736b
Thomas Schmitt <schmitt@lmz-bw.de>
2012-01-17 21:10:47 +0000

    * linuxmuster-setup:    * Weiteren Rembo-Code entfernt.    * Postgresqls pg_hba.conf gefixt.    * Netzwerkkartenmangement überarbeitet.

--- share/scripts/nic_setup.sh
+++ share/scripts/nic_setup.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+# assign specific nic to lan
+# $Id$
 
 # Source debconf library.
 . /usr/share/debconf/confmodule
@@ -15,25 +17,8 @@ PRIORITY="critical"
 
 db_title "$(getdistname) $DISTFULLVERSION"
 
-# read fwconfig from debconf
-db_get linuxmuster-base/fwconfig || true
-fwconfig=$RET
-if [ -z "$fwconfig" ]; then
-	echo "Cannot determine firewall type. Aborting!"
-	exit 1
-fi
-
-# assign nics with extern, intern, wlan, dmz interfaces
 assign_nics
 
 # important: close debconf database
 db_stop
 
-# write macs to network.settings
-grep -v ^mac_ $NETWORKSETTINGS > $NETWORKSETTINGS.tmp
-mv $NETWORKSETTINGS.tmp $NETWORKSETTINGS
-echo "mac_extern=$mac_extern" >> $NETWORKSETTINGS
-echo "mac_intern=$mac_intern" >> $NETWORKSETTINGS
-echo "mac_wlan=$mac_wlan" >> $NETWORKSETTINGS
-echo "mac_dmz=$mac_dmz" >> $NETWORKSETTINGS
-chmod 755 $NETWORKSETTINGS
