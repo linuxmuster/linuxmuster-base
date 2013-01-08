e2eace4a454516c3bd9a427e33afdef87bee02e3
Thomas Schmitt <tschmitt@schmitt-zkn.(none)>
2013-01-08 16:25:09 +0100

    * wimport.sh:   - Weitere Optimierungen bzgl. Konsolenausgabe und Programmablauf,     z.B. Abbruch bei ungültiger MAC-Adresse.   - Alten Programmcode entfernt. * Firewall-Initskript, reload_firewall(): kleine Verbesserungen   bzgl. Fehlerbehandlung und Manipulation der Iptablesdatei. * blocked_ports: obsoleten Webminport entfernt, Portfolge   "ftp-data,ftp,ssh" in Portbereich "ftp-data:ssh" geändert,   was Regeln einspart.

--- share/scripts/wimport.sh
+++ share/scripts/wimport.sh
@@ -1,37 +1,15 @@
 # workstation import for linuxmuster.net
 #
-# tschmitt 20130103
+# Thomas Schmitt <thomas@linuxmuster.net>
+# 07.01.2013
 # GPL v3
 #
-
-WDATATMP=/var/tmp/workstations.$$
-[ -e "$WDATATMP" ] && rm -rf $WDATATMP
 
 DB10TMP=/var/tmp/db10.$$
 DBREVTMP=/var/tmp/dbrev.$$
 
-# deprecated
-#MACHINE_PASSWORD=12345678
-#HOST_PASSWORD=`pwgen -s 8 1`
-#QUOTA=`grep ^'\$use_quota' $SOPHOMORIXCONF | awk -F\" '{ print $2 }' | tr A-Z a-z`
-
-# get host and machine accounts
-#echo -n "Reading account data ."
-#HOSTS_DB="$(hosts_db)"
-#echo -n .
-#HOSTS_LDAP="$(hosts_ldap)"
-#echo -n .
-#MACHINES_DB="$(machines_db)"
-#echo -n .
-#MACHINES_LDAP="$(machines_ldap)"
-#echo -n .
-#ACCOUNTS_DB="$(accounts_db)"
-#echo -n .
-#ACCOUNTS_LDAP="$(accounts_ldap)"
-#echo " Done!"
-#echo
-
 RC=0
+
 
 # functions
 # check for unique entry
@@ -47,137 +25,26 @@ check_unique() {
  return 0
 }
 
+
 # cancel with message
 exitmsg() {
  echo "  > $1"
  echo
  echo "An error ocurred and import_workstations will be cancelled!"
  echo "No modifications have been made to your system!"
- rm $WDATATMP
  rm -f $locker
  RC=1
  echo
  exit $RC
 }
 
-# checking for valid host/machine account, returns 0 if no account exists
-#check_host_account() {
-# echo "$HOSTS_LDAP" | grep -qw "$hostname" || return 1
-# echo "$MACHINES_LDAP" | grep -qw "${hostname}\\$" || return 1
-# echo "$HOSTS_DB" | grep -qw "$hostname" || return 1
-# echo "$MACHINES_DB" | grep -qw "${hostname}\\$" || return 1
-# return 0
-#}
-
-# checking for valid user account, returns 0 if no account exists
-#check_user_account() {
-# if echo "$ACCOUNTS_LDAP" | grep -qw "$hostname"; then
-#  RET=ldap
-#  return 1
-# fi
-# if echo "$ACCOUNTS_DB" | grep -qw "$hostname"; then
-#  RET=postgresql
-#  return 1
-# fi
-# if grep -q ^"${hostname}"\: /etc/passwd; then
-#  RET=system
-#  return 1
-# fi
-# RET=""
-# return 0
-#}
-
-# create workstation and machine accounts
-#create_account() {
-# if [ -e "$SOPHOMORIXLOCK" ]; then
-#  echo "  > Error: Sophomorix lockfile $SOPHOMORIXLOCK detected!"
-#  return 1
-# fi
-# echo -n "  * Creating exam account $hostname ... "
-# if sophomorix-useradd --examaccount $hostname --unix-group $room 2>> $TMPLOG 1>> $TMPLOG; then
-#  echo "Ok!"
-# else
-#  echo "sophomorix error!"
-#  return 1
-# fi
-# echo -n "  * Setting exam account password for ${hostname} ... "
-# if sophomorix-passwd -u $hostname --pass $HOST_PASSWORD 2>> $TMPLOG 1>> $TMPLOG; then
-#  echo "Ok!"
-# else
-#  echo "sophomorix error!"
-#  return 1
-# fi
-# local homedir="$WSHOME/$room/$hostname"
-# if [ ! -d "$homedir" ]; then
-#  mkdir -p "$homedir"
-#  chown ${hostname}:${TEACHERSGROUP} "$homedir"
-#  chmod 775 "$homedir"
-# fi
-# if [ "$QUOTA" = "yes" ]; then
-#  echo -n "  * Setting quota for $hostname ... "
-#  if sophomorix-quota -u $hostname 2>> $TMPLOG 1>> $TMPLOG; then
-#   echo "Ok!"
-#  else
-#   echo "sophomorix error!"
-#   return 1
-#  fi
-# fi
-# echo -n "  * Creating machine account ${hostname}$ ... "
-# if sophomorix-useradd --computer ${hostname}$ 2>> $TMPLOG 1>> $TMPLOG; then
-#  echo "Ok!"
-# else
-#  echo "sophomorix error!"
-#  return 1
-# fi
-# echo -n "  * Setting machine account password for ${hostname}$ ... "
-# if sophomorix-passwd --force -u ${hostname}$ --pass $MACHINE_PASSWORD 2>> $TMPLOG 1>> $TMPLOG; then
-#  echo "Ok!"
-# else
-#  echo "sophomorix error!"
-#  return 1
-# fi
- # disable password change
-# smbldap-usermod -A0 -B0 ${hostname}$ 2>> $TMPLOG 1>> $TMPLOG
-#}
-
-# remove workstation and machine accounts
-#remove_account() {
-# if ! check_host_account $hostname; then
-#  if check_user_account; then
-#   echo "  * Removing orphaned computer account home directory: $hostdir."
-#   rm -rf $hostdir
-#   return 0
-#  fi
-#  echo "  > Error: $hostname is an existing $RET user account! Not removing!"
-#  return 1
-# fi
-# if [ -e "$SOPHOMORIXLOCK" ]; then
-#  echo "  > Error: Sophomorix lockfile $SOPHOMORIXLOCK detected!"
-#  return 1
-# fi
-# echo -n "  * Removing exam account $hostname ... "
-# if sophomorix-kill --killuser $hostname 2>> $TMPLOG 1>> $TMPLOG; then
-#  [ -d "$i" ] && rm -rf $i 2>> $TMPLOG 1>> $TMPLOG
-#  echo "Ok!"
-# else
-#  echo "sophomorix error!"
-#  return 1
-# fi
-# echo -n "  * Removing machine account ${hostname}$ ... "
-# if sophomorix-kill --killuser ${hostname}$ 2>> $TMPLOG 1>> $TMPLOG; then
-#  echo "Ok!"
-# else
-#  echo "sophomorix error!"
-#  return 1
-# fi
-#}
 
 # remove room or host from $PRINTERS
 remove_printeraccess() {
  local toremove=$1
  local PRINTERSTMP=/var/tmp/printers.$$
  [ -e "$PRINTERSTMP" ] && rm -rf $PRINTERSTMP
- echo -n "  * Removing $toremove from $PRINTERS ... "
+ echo -n " * Removing $toremove from $PRINTERS ... "
  while read line; do
   if [ "${line:0:1}" = "#" ]; then
    echo "$line" >> $PRINTERSTMP
@@ -209,11 +76,12 @@ remove_printeraccess() {
  echo "Ok!"
 }
 
+
 # remove deleted rooms or hosts from $ROOMDEFAULTS
 remove_defaults() {
  local toremove=$1
  local RC=0
- echo -n "  * Removing $toremove from $ROOMDEFAULTS ... "
+ echo -n " * Removing $toremove from $ROOMDEFAULTS ... "
  backup_roomdefaults=yes
  grep -v ^$toremove[[:space:]] $ROOMDEFAULTS > $ROOMDEFAULTS.tmp; RC=$?
  mv $ROOMDEFAULTS.tmp $ROOMDEFAULTS; RC=$?
@@ -242,21 +110,14 @@ if ls $LINBODIR/*.new &> /dev/null; then
 fi
 
 
-# check for bad workstation data and cancel processing if necessary
-touch $WDATATMP
+# only if there are any workstation data
 if [ -s "$WIMPORTDATA" ]; then
 
- # create a clean workstation data file
- RC_LINE=0
+ # check for bad workstation data and cancel processing if necessary
  echo "Checking workstation data:"
  while read line; do
 
-  # skip comment lines
-  [ "${line:0:1}" = "#" ] && continue
-
-  # strip spaces and skip empty lines
-  line=${line// /}
-  [ -z "$line" ] && continue
+  echo "$line" | grep -q ^[a-zA-Z0-9] || continue
 
   room_orig=`echo $line | awk -F\; '{ print $1 }'`
   if ! check_string "$room_orig"; then
@@ -277,11 +138,6 @@ if [ -s "$WIMPORTDATA" ]; then
    [ -z "$hostname" ] && hostname="<empty>"
    exitmsg "$hostname is no valid hostname!"
   fi
-  # check if hostname exists already as a user account
-  #if ! check_user_account; then
-  # exitmsg "Hostname $hostname exists already as a $RET user account!"
-  #fi
-  # check for uppercase hostnames
   if [ "$hostname_orig" != "$hostname" ]; then
    hostnames_to_be_converted="$hostnames_to_be_converted $hostname_orig"
   fi
@@ -295,9 +151,7 @@ if [ -s "$WIMPORTDATA" ]; then
   mac_orig=`echo $line | awk -F\; '{ print $4 }'`
   if ! validmac "$mac_orig"; then
    [ -z "$mac_orig" ] && mac_orig="<empty>"
-   echo "  > $mac_orig is no valid mac address! Skipping $hostname."
-   RC_LINE=1
-   continue
+   exitmsg "Host $hostname: $mac_orig is no valid mac address!"
   fi
   toupper $mac_orig
   mac=$RET
@@ -317,71 +171,63 @@ if [ -s "$WIMPORTDATA" ]; then
    exitmsg "Host $hostname: PXE-Flag is not set!"
   fi
 
-  echo "$room;$hostname;$hostgroup;$mac;$ip;$internmask;1;1;1;1;$pxe" >> $WDATATMP
-
  done <$WIMPORTDATA
 
-fi
-
-# check for repeated hostnames, macs and ips
-if [ -s "$WDATATMP" ]; then
-
  # check hostnames
- hostnames=`awk -F\; '{ print $2 }' $WDATATMP`
+ hostnames=`awk -F\; '{ print $2 }' $WIMPORTDATA`
  for i in $hostnames; do
   check_unique "$i" "$hostnames" || exitmsg "Hostname $i is not unique!"
  done
 
  # check macs
- macs=`awk -F\; '{ print $4 }' $WDATATMP`
+ macs=`awk -F\; '{ print $4 }' $WIMPORTDATA`
  for i in $macs; do
   check_unique "$i" "$macs" || exitmsg "MAC address $i is not unique!"
  done
-
+ 
  # check ips
- ips=`awk -F\; '{ print $5 }' $WDATATMP`
+ ips=`awk -F\; '{ print $5 }' $WIMPORTDATA`
  for i in $ips; do
   check_unique "$i" "$ips" || exitmsg "IP address $i is not unique!"
  done
 
-fi
+ # convert lowercase macs
+ if [ -n "$macs_to_be_converted" ]; then
+  for i in $macs_to_be_converted; do
+   toupper $i
+   sed -e "s|$i|$RET|g" -i "$WIMPORTDATA"
+  done
+ fi
 
-# convert lowercase macs
-if [ -n "$macs_to_be_converted" ]; then
- for i in $macs_to_be_converted; do
-  toupper $i
-  sed -e "s|$i|$RET|g" -i "$WIMPORTDATA"
- done
-fi
+ # convert uppercase room names
+ if [ -n "$rooms_to_be_converted" ]; then
+  for i in $rooms_to_be_converted; do
+   tolower $i
+   sed -e "s|^$i\;|$RET\;|g
+           s|^$i |$RET |g" -i "$WIMPORTDATA"
+  done
+ fi
 
-# convert uppercase room names
-if [ -n "$rooms_to_be_converted" ]; then
- for i in $rooms_to_be_converted; do
-  tolower $i
-  sed -e "s|^$i\;|$RET\;|g
-          s|^$i |$RET |g" -i "$WIMPORTDATA"
- done
-fi
+ # convert uppercase hostnames
+ if [ -n "$hostnames_to_be_converted" ]; then
+  for i in $hostnames_to_be_converted; do
+   tolower $i
+   sed -e "s|\;$i\;|\;$RET\;|g
+           s|\;$i |\;$RET |g
+           s| $i | $RET |g
+           s| $i\;| $RET\;|g" -i "$WIMPORTDATA"
+  done
+ fi
 
-# convert uppercase hostnames
-if [ -n "$hostnames_to_be_converted" ]; then
- for i in $hostnames_to_be_converted; do
-  tolower $i
-  sed -e "s|\;$i\;|\;$RET\;|g
-          s|\;$i |\;$RET |g
-          s| $i | $RET |g
-          s| $i\;| $RET\;|g" -i "$WIMPORTDATA"
- done
-fi
+ echo " * Workstation data are Ok!"
+ echo
 
-# evaluate workstation data checks
-[ "$RC_LINE" = "0" -a -s "$WDATATMP" ] && echo "  * Workstation data are Ok! :-)"
-if [ "$RC_LINE" != "0" ]; then
- RC=1
- echo "  > Workstation data with errors! :-("
-fi
-[ -s "$WDATATMP" ] || echo "  * No valid workstation data found! Skipping workstation import!"
-echo
+else
+
+ echo " * No valid workstation data found! Skipping workstation import!"
+ echo
+
+fi # [ -s "$WIMPORTDATA" ]
 
 # check dhcp stuff
 [ -z "$DHCPDCONF" ] && exitmsg "Variable DHCPDCONF is not set!"
@@ -390,6 +236,22 @@ if [ -e "$DHCPDCONF" ]; then
  rm -rf $DHCPDCONF 2>> $TMPLOG 1>> $TMPLOG || exitmsg "Unable to delete $DHCPDCONF!"
 fi
 touch $DHCPDCONF 2>> $TMPLOG 1>> $TMPLOG || exitmsg "Unable to create $DHCPDCONF!"
+
+
+# sync host accounts
+echo "Sophomorix syncs accounts (may take a while):"
+sophomorix-workstation --sync-accounts | grep ^[KA][id][ld] 2>> $TMPLOG ; RC_LINE="${PIPESTATUS[0]}"
+if [ "$RC_LINE" = "0" ]; then
+ echo "Done!"
+ echo
+else
+ RC="$RC_LINE"
+ echo "sophomorix-workstation exits with error!"
+ echo
+ rm -f $locker
+ exit "$RC"
+fi # sync host accounts
+
 
 # remove host entries from bind config
 backup_file $DB10 2>> $TMPLOG 1>> $TMPLOG || exitmsg "Unable to backup $DB10!"
@@ -400,130 +262,90 @@ removefrom_file $DBREV "/\;$BEGINSTR/,/\;$ENDSTR/"
 echo ";$BEGINSTR" > $DB10TMP
 echo ";$BEGINSTR" > $DBREVTMP
 
-# read in rooms
-# rooms=`ls $WSHOME/`
 
+ # if there are any workstation data
+if [ -s "$WIMPORTDATA" ]; then
 
-# if there are any workstation data
-if [ -s "$WDATATMP" ]; then
+ # remove old links
+ echo -n "Removing old start.conf links ... "
+ find "$LINBODIR" -name "start.conf-*" -type l -exec rm '{}' \;
+ echo "Done!"
+ echo
 
- # sync host accounts
- echo "Sophomorix syncs accounts (may take a while):"
- if sophomorix-workstation --sync-accounts | grep ^[KA][id][ld] | tee -a $TMPLOG 2>> $TMPLOG; then
+ # write configuration files
+ while read line; do
 
-  echo "Done!"
-  echo
+  echo "$line" | grep -q ^[a-zA-Z0-9] || continue
+  
+  # read in host data
+  room=`echo $line | awk -F\; '{ print $1 }'`
+  tolower $room
+  room=$RET
+  hostname=`echo $line | awk -F\; '{ print $2 }'`
+  hostgroup=`echo $line | awk -F\; '{ print $3 }'`
+  hostgroup=`echo $hostgroup | awk -F\, '{ print $1 }'`
+  mac=`echo $line | awk -F\; '{ print $4 }'`
+  ip=`echo $line | awk -F\; '{ print $5 }'`
+  pxe=`echo $line | awk -F\; '{ print $11 }'`
+  echo "Processing host $hostname:"
 
-  # remove old links
-  echo -n "Removing old start.conf links ... "
-  find "$LINBODIR" -name "start.conf-*" -type l -exec rm '{}' \;
-  echo "Done!"
-  echo
+  # only if pxe host
+  if [ "$pxe" != "0" ]; then
 
-  # write configuration files
-  while read line; do
-
-#  RC_LINE=0
-
-   # read in host data
-   room=`echo $line | awk -F\; '{ print $1 }'`
-   tolower $room
-   room=$RET
-   hostname=`echo $line | awk -F\; '{ print $2 }'`
-   hostgroup=`echo $line | awk -F\; '{ print $3 }'`
-   hostgroup=`echo $hostgroup | awk -F\, '{ print $1 }'`
-   mac=`echo $line | awk -F\; '{ print $4 }'`
-   ip=`echo $line | awk -F\; '{ print $5 }'`
-   pxe=`echo $line | awk -F\; '{ print $11 }'`
-   echo "Processing host $hostname:"
-
-   # deprecated because of sophomorix-workstations
-   # create workstation and machine accounts
-#  if check_host_account; then
-#   get_pgroup $hostname
-#   strip_spaces $RET
-#   pgroup=$RET
-#   if [ "$pgroup" != "$room" ]; then
-#    echo "  * Host $hostname is moving from room $pgroup to $room!"
-#    remove_account; RC_LINE=$?
-#    if [ $RC_LINE -eq 0 ]; then
-#     create_account; RC_LINE=$?
-#    fi
-#   fi
-#  else
-#   create_account; RC_LINE=$?
-#  fi
-
-#  if [ $RC_LINE -ne 0 ]; then
-#   echo
-#   RC=$RC_LINE
-#   continue
-#  fi
-
-   # only if pxe host
-   if [[ "$pxe" != "0" ]]; then
-
-    # use the default start.conf if there is none for this group
-    if [ ! -e "$LINBODIR/start.conf.$hostgroup" ]; then
-     echo -n "  * LINBO: Creating new start.conf.$hostgroup in $LINBODIR ... "
-     if cp $LINBODEFAULTCONF $LINBODIR/start.conf.$hostgroup; then
-      sed -e "s/^Server.*/Server = $serverip/
-              s/^Description.*/Description = Windows XP/
-              s/^Image.*/Image =/
-              s/^BaseImage.*/BaseImage = winxp-$hostgroup.cloop/" -i $LINBODIR/start.conf.$hostgroup
-      echo "Ok!"
-     else
-      echo "Error!"
-      RC=1
-     fi
+   # use the default start.conf if there is none for this group
+   if [ ! -e "$LINBODIR/start.conf.$hostgroup" ]; then
+    echo -n " * LINBO: Creating new start.conf.$hostgroup in $LINBODIR ... "
+    if cp $LINBODEFAULTCONF $LINBODIR/start.conf.$hostgroup; then
+     sed -e "s/^Server.*/Server = $serverip/
+             s/^Description.*/Description = Windows XP/
+             s/^Image.*/Image =/
+             s/^BaseImage.*/BaseImage = winxp-$hostgroup.cloop/" -i $LINBODIR/start.conf.$hostgroup
+     echo "Ok!"
+    else
+     echo "Error!"
+     RC=1
     fi
-
-    echo -n "  * LINBO: Linking $ip to group $hostgroup ... "
-    ln -sf start.conf.$hostgroup $LINBODIR/start.conf-$ip
-
-    # if there is no pxelinux boot file for the group
-    if [ ! -s "$LINBODIR/pxelinux.cfg/$hostgroup" ]; then
-     # create one
-     sed -e "s/initrd=linbofs.gz/initrd=linbofs.$hostgroup.gz/g" $PXELINUXCFG > $LINBODIR/pxelinux.cfg/$hostgroup
-    fi
-
-    echo "Ok!"
-
-   fi # only if pxe host
-
-   # write dhcpd.conf entry
-   echo -n "  * DHCP/BIND: Writing config ... "
-   echo "host $hostname {" >> $DHCPDCONF
-   echo "  hardware ethernet $mac;" >> $DHCPDCONF
-   echo "  fixed-address $ip;" >> $DHCPDCONF
-   echo "  option host-name \"$hostname\";" >> $DHCPDCONF
-   if [[ "$pxe" != "0" && "$imaging" = "linbo" ]]; then
-    # assign group specific pxelinux config
-    echo "  option pxelinux.configfile \"pxelinux.cfg/$hostgroup\";" >> $DHCPDCONF
    fi
-   echo "}" >> $DHCPDCONF
-		
-   # write bind config
-   okt2="$(echo $ip | awk -F. '{ print $2 }')"
-   okt3="$(echo $ip | awk -F. '{ print $3 }')"
-   okt4="$(echo $ip | awk -F. '{ print $4 }')"
-   echo "$okt4.$okt3.$okt2 PTR $hostname.`dnsdomainname`." >> $DB10TMP
-   echo "$hostname A $ip" >> $DBREVTMP
+
+   echo -n " * LINBO: Linking $ip to group $hostgroup ... "
+   ln -sf start.conf.$hostgroup $LINBODIR/start.conf-$ip
+
+   # if there is no pxelinux boot file for the group
+   if [ ! -s "$LINBODIR/pxelinux.cfg/$hostgroup" ]; then
+    # create one
+    sed -e "s/initrd=linbofs.gz/initrd=linbofs.$hostgroup.gz/g" $PXELINUXCFG > $LINBODIR/pxelinux.cfg/$hostgroup
+   fi
 
    echo "Ok!"
-   echo
 
-  done <$WDATATMP
+  fi # only if pxe host
 
- else
+  # write dhcpd.conf entry
+  echo -n " * DHCP/BIND: Writing config ... "
+  echo "host $hostname {" >> $DHCPDCONF
+  echo "  hardware ethernet $mac;" >> $DHCPDCONF
+  echo "  fixed-address $ip;" >> $DHCPDCONF
+  echo "  option host-name \"$hostname\";" >> $DHCPDCONF
+  if [ "$pxe" != "0" -a "$imaging" = "linbo" ]; then
+   # assign group specific pxelinux config
+   echo "  option pxelinux.configfile \"pxelinux.cfg/$hostgroup\";" >> $DHCPDCONF
+  fi
+  echo "}" >> $DHCPDCONF
+		
+  # write bind config
+  okt2="$(echo $ip | awk -F. '{ print $2 }')"
+  okt3="$(echo $ip | awk -F. '{ print $3 }')"
+  okt4="$(echo $ip | awk -F. '{ print $4 }')"
+  echo "$okt4.$okt3.$okt2 PTR $hostname.`dnsdomainname`." >> $DB10TMP
+  echo "$hostname A $ip" >> $DBREVTMP
 
-  echo "Error!"
+  echo "Ok!"
   echo
-  RC=1
 
- fi # sync host accounts
+ done <$WIMPORTDATA
 
 fi # if there are any workstation data
+
 
 # finalize bind config
 echo ";$ENDSTR" >> $DB10TMP
@@ -556,7 +378,7 @@ echo "Checking for obsolete hosts:"
 hosts="$(grep ^[a-z0-9] $ROOMDEFAULTS | awk '{ print $1 }' | tr A-Z a-z)"
 for hostname in $hosts; do
  [ "$hostname" = "default" ] && continue
- if ! awk -F\; '{ print $2 }' $WDATATMP | sort -u | grep -qw $hostname; then
+ if ! awk -F\; '{ print $2 }' $WIMPORTDATA | sort -u | grep -qw $hostname; then
   FOUND=1
   remove_defaults $hostname ; RC_LINE="$?"
   [ $RC_LINE -eq 0 ] || RC=1
@@ -565,12 +387,13 @@ done
 # printers
 hosts="$(grep ^[a-zA-Z0-9] $PRINTERS | awk '{ print $3 }' | grep ^[a-z0-9] | sed -e 's|,| |g')"
 for hostname in $hosts; do
- if ! awk -F\; '{ print $2 }' $WDATATMP | sort -u | grep -qw $hostname; then
+ if ! awk -F\; '{ print $2 }' $WIMPORTDATA | sort -u | grep -qw $hostname; then
   FOUND=1
   remove_printeraccess $hostname
  fi
 done
-[ "$FOUND" = "0" ] && echo "  * Nothing to do!"
+[ "$FOUND" = "0" ] && echo " * Nothing to do!"
+
 
 # remove rooms which are no more defined in workstations file
 FOUND=0
@@ -579,9 +402,9 @@ echo "Checking for obsolete rooms:"
 # classrooms
 rooms="$(grep ^[a-z0-9] $CLASSROOMS | awk '{ print $1 }' | tr A-Z a-z)"
 for room in $rooms; do
- if ! grep -q ^${room}\; $WDATATMP; then
+ if ! grep -q ^${room}\; $WIMPORTDATA; then
   FOUND=1
-  echo -n "  * Removing $room from $CLASSROOMS ... "
+  echo -n " * Removing $room from $CLASSROOMS ... "
   backup_classrooms=yes
   grep -wv ^$room $CLASSROOMS > $CLASSROOMS.tmp
   mv $CLASSROOMS.tmp $CLASSROOMS
@@ -592,7 +415,7 @@ done
 rooms="$(grep ^[a-z0-9] $ROOMDEFAULTS | awk '{ print $1 }' | tr A-Z a-z)"
 for room in $rooms; do
  [ "$room" = "default" ] && continue
- if ! awk -F\; '{ print $1 }' $WDATATMP | sort -u | grep -qw $room; then
+ if ! awk -F\; '{ print $1 }' $WIMPORTDATA | sort -u | grep -qw $room; then
   FOUND=1
   remove_defaults $room ; RC_LINE="$?"
   [ $RC_LINE -eq 0 ] || RC=1
@@ -601,12 +424,12 @@ done
 # printers
 rooms="$(grep ^[a-zA-Z0-9] $PRINTERS | awk '{ print $2 }' | grep ^[a-z0-9] | sed -e 's|,| |g')"
 for room in $rooms; do
- if ! awk -F\; '{ print $1 }' $WDATATMP | sort -u | grep -qw $room; then
+ if ! awk -F\; '{ print $1 }' $WIMPORTDATA | sort -u | grep -qw $room; then
   FOUND=1
   remove_printeraccess $room
  fi
 done
-[ "$FOUND" = "0" ] && echo "  * Nothing to do!"
+[ "$FOUND" = "0" ] && echo " * Nothing to do!"
 
 
 # remove backup files if nothing was changed
@@ -625,9 +448,10 @@ echo "   ...done."
 /etc/init.d/bind9 force-reload
 [ -n "$update_printers" ] && import_printers
 
+
 # delete tmp files
-rm $WDATATMP
 [ -n "$PRINTERSTMP" ] && [ -e "$PRINTERSTMP" ] && rm -rf $PRINTERSTMP
+
 
 # exit with return code
 exit $RC
