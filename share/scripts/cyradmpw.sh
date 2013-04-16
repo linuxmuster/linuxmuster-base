1ebbd13f240efb864d1456ea68f2b850beace750
Thomas Schmitt <thomas@linuxmuster.net>
2013-04-16 22:09:55 +0200

    * system user Passwörter auf 24 Zeichen vergrößert (schließt #75). * restart_printer_queues.sh: Druckernamenbestimmung aus printers.conf gefixt.

--- share/scripts/cyradmpw.sh
+++ share/scripts/cyradmpw.sh
@@ -2,12 +2,13 @@
 #
 # sets a new random password for cyrus user
 #
-# Thomas Schmitt
-# <tschmitt@linuxmuster.de>
+# <thomas@linuxmuster.net>
+# 16.04.2013
+# GPL v3
 #
 
-cyradmpw=`pwgen -s 8 1`
+cyradmpw=`pwgen -s 24 1`
 echo "$cyradmpw" | saslpasswd2 -p -c cyrus
 echo "cyrus:$cyradmpw" | chpasswd
-echo $cyradmpw > /etc/imap.secret
+echo "$cyradmpw" > /etc/imap.secret
 chmod 600 /etc/imap.secret
