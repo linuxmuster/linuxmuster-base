667ec76ea73ebc69f1b4c06b438612a44da70483
Frank Schütte <fschuett@gymnasium-himmelsthuer.de>
2016-08-08 19:06:37 +0200

    move horde to separate package linuxmuster-horde

--- var/config-static/21_horde/etc/linuxmuster/horde/horde/prefs.local.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// kopierte portalseite
-$_prefs['portal_layout']['value'] = 'a:2:{i:0;a:2:{i:0;a:4:{s:3:"app";s:3:"imp";s:6:"height";i:1;s:5:"width";i:1;s:6:"params";a:2:{s:5:"type2";s:17:"imp_Block_Newmail";s:6:"params";a:1:{s:10:"msgs_shown";i:3;}}}i:1;a:4:{s:3:"app";s:9:"kronolith";s:6:"height";i:1;s:5:"width";i:1;s:6:"params";a:2:{s:5:"type2";s:25:"kronolith_Block_Monthlist";s:6:"params";a:4:{s:8:"calendar";s:5:"__all";s:6:"months";i:2;s:9:"maxevents";i:0;s:6:"alarms";i:0;}}}}i:1;a:2:{i:0;a:4:{s:3:"app";s:4:"ingo";s:6:"height";i:1;s:5:"width";i:1;s:6:"params";a:2:{s:5:"type2";s:19:"ingo_Block_Overview";s:6:"params";a:0:{}}}i:1;a:4:{s:3:"app";s:3:"nag";s:6:"height";i:1;s:5:"width";i:1;s:6:"params";a:2:{s:5:"type2";s:17:"nag_Block_Summary";s:6:"params";a:9:{s:11:"block_title";s:8:"Aufgaben";s:8:"show_pri";i:1;s:12:"show_actions";i:1;s:8:"show_due";i:1;s:13:"show_tasklist";i:1;s:11:"show_alarms";i:1;s:12:"show_overdue";i:1;s:14:"show_completed";i:1;s:14:"show_tasklists";a:0:{}}}}}}';
-
-// hooks für identität
-$_prefs['from_addr']['hook'] = true;
-$_prefs['fullname']['hook'] = true;
